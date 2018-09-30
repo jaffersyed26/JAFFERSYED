e46f979acca2325e8e814506652f68b4cb35bb32
jaffersyed26 <35580379+jaffersyed26@users.noreply.github.com>
2018-09-29 21:14:26 -0400

    commit the inital draft

---
+++ index.js
@@ -0,0 +1,25 @@
+document.querySelector('#nav-btn-mobile').addEventListener('click', function(){
+   var navMobile = document.querySelector('#dropup-links');
+      
+   if (navMobile.style.display === 'none') {
+      navMobile.style.display = 'block'
+   } else {
+       navMobile.style.display = 'none'
+   }
+
+})
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+
+  $(function () {
+    $(document).scroll(function () {
+        var $nav = $("#navbar2");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+
