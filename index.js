343649d0f3c169f023a9bb0981f431e2b30f9854
jaffersyed26 <35580379+jaffersyed26@users.noreply.github.com>
2018-11-25 16:43:12 -0500

    still have to fix navabr

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ $(function () {
       });
   });
 
+  $(function () {
+    $(document).scroll(function () {
+        var $nav = $("#navbar-dropdown");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+
+  function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+
+
 
 var tot = document.getElementById('tot');
 var iswaf = document.getElementById('iswaf');
@@ -33,3 +46,17 @@ iswaf.addEventListener('click' , iswafDirect);
 xpress.addEventListener('click', xpressDirect);
 novacore.addEventListener('click', novacoreDirect);
 
+var dropdownBtn =  document.querySelector('.drop-btn');
+
+
+function show() {
+    var dropdown = document.querySelector('#dropdown-menu');
+
+    if (dropdown.style.display === 'none') {
+        dropdown.style.display = 'block'
+    } else {
+        dropdown.style.display === 'none'
+    }
+}
+
+dropdownBtn.addEventListener('click', show);
